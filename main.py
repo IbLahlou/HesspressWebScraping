6e65dceb814a5441fa9bdd0a04fcba173c051c67
Ibrahim Lahlou <ibrahimlahlou021@gmail.com>
2022-08-23 14:47:43 +0100

    Importation des Bibliothèque

---
+++ main.py
@@ -0,0 +1,6 @@
+#Importation des Bibliothèque
+from bs4 import BeautifulSoup
+import requests
+from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import Font
