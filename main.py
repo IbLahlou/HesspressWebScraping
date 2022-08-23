af521b672f5cd33f75cf241143e916a815dea0ec
Ibrahim Lahlou <ibrahimlahlou021@gmail.com>
2022-08-23 14:52:18 +0100

    Création d'un Fichier Excel

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ import requests
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font
+
+#Creation d'un Fichier Excel avec les titres de chaque colonne
+wb = Workbook()
+ws = wb.active
+ws.title = 'Posts'
+
+heading = ['Title', 'Category', 'Date', 'Content', 'Link', 'Img']
+ws.append(heading)
